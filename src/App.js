4da042b58e37f32a8fc6836f0408a17094548b68
Ian Guy <iguy@udel.edu>
2025-01-10 19:09:37 -0500

    nice nice

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import lebron from './images/lebron.gif';
+
+function App(){
+    return(
+    <div>
+   <h1>Welcome to my Personal Portfolio page!</h1>
+   <h2>Please enjoy this picture of my goat.</h2>
+   <img src={lebron} alt="Lebron" />
+  </div>
+    );
+}
+
+export default App;  
