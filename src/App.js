cc95ece9426a222230f8bbd7379a076f97b9e057
Ian Guy <iguy@udel.edu>
2025-01-10 19:10:56 -0500

    lebronnnnnn

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import lebron from './images/lebron.gif';
 function App(){
     return(
     <div>
-   <h1>Welcome to my Personal Portfolio page!</h1>
+   <h1>Welcome to my Personal Portfolio Page!</h1>
    <h2>Please enjoy this picture of my goat.</h2>
    <img src={lebron} alt="Lebron" />
   </div>
